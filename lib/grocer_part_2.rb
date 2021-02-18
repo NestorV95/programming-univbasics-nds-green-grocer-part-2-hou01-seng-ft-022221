b1ae83ab83217786e7144ac851c2546e8be00e0b
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-18 18:27:18 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -9,7 +9,7 @@ def apply_coupons(cart, coupons)
     cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
      if cart_item && cart_item[:count] >= coupons[count][:num]
       if cart_item_with_coupon
-        cart_item_with_coupon[:count] += coupons[count]
+        cart_item_with_coupon[:count] += coupons[count][:num]
   count +=1 
  end 
 end
